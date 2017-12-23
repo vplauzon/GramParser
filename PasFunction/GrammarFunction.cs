b20b8a3445c6444802c81c9166a8252f8ca507fa
Vincent-Philippe Lauzon <vilauzon@microsoft.com>
2017-12-22 21:06:41 -0500

    .

---
+++ PasFunction/GrammarFunction.cs
@@ -0,0 +1,23 @@
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json;
+
+namespace PasFunction
+{
+    public static class GrammarFunction
+    {
+        [FunctionName("GrammarFunction")]
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)]HttpRequest req, TraceWriter log)
+        {
+            log.Info("Begin request.");
+
+            return new OkObjectResult($"Hello world");
+                //: new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+        }
+    }
+}
