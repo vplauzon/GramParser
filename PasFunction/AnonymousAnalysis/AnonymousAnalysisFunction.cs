953258be8f23e27ddefc85ce51617f0b3ac573d7
Vincent-Philippe Lauzon <vilauzon@microsoft.com>
2017-12-29 10:43:31 -0500

    Log errors

--- PasFunction/AnonymousAnalysis/AnonymousAnalysisFunction.cs
+++ PasFunction/AnonymousAnalysis/AnonymousAnalysisFunction.cs
@@ -21,56 +21,69 @@ namespace PasFunction.AnonymousAnalysis
         {
             try
             {
-                log.Info("Begin request.");
-
                 var body =
                     JsonSerializerHelper.Deserialize<AnonymousAnalysisInputModel>(req.Body);
 
-                if (string.IsNullOrWhiteSpace(body.Grammar))
+                try
                 {
-                    return new BadRequestObjectResult("JSON body should contain 'grammar'");
+
+                    if (string.IsNullOrWhiteSpace(body.Grammar))
+                    {
+                        return new BadRequestObjectResult("JSON body should contain 'grammar'");
+                    }
+                    if (string.IsNullOrWhiteSpace(body.Text))
+                    {
+                        return new BadRequestObjectResult("JSON body should contain 'text'");
+                    }
+
+                    var grammar = MetaGrammar.ParseGrammar(body.Grammar);
+
+                    if (grammar == null)
+                    {
+                        return new BadRequestObjectResult("Grammar cannot be parsed");
+                    }
+
+                    var match = grammar.Match(body.Rule, body.Text);
+
+                    if (match == null)
+                    {
+                        return new BadRequestObjectResult("Text cannot be matched by grammar");
+                    }
+
+                    var outputModel = new AnonymousAnalysisOutputModel(match);
+                    var output = JsonSerializerHelper.Serialize(outputModel);
+
+                    return new ContentResult
+                    {
+                        StatusCode = 200,
+                        ContentType = "application/json",
+                        Content = output
+                    };
                 }
-                if (string.IsNullOrWhiteSpace(body.Text))
+                catch (ParsingException ex)
                 {
-                    return new BadRequestObjectResult("JSON body should contain 'text'");
+                    return new BadRequestObjectResult(ex.Message);
                 }
-
-                var grammar = MetaGrammar.ParseGrammar(body.Grammar);
-
-                if (grammar == null)
+                catch (Exception ex)
                 {
-                    return new BadRequestObjectResult("Grammar cannot be parsed");
+                    log.Error($"Internal Error:  {ex.GetType().Name}, '{ex.Message}'");
+                    log.Info($"Grammar:  '{body.Grammar}'");
+                    log.Info($"Text:  '{body.Text}'");
+                    log.Info($"Rule:  '{body.Rule}'");
+
+                    return new ContentResult
+                    {
+                        StatusCode = 500,
+                        ContentType = "text/plain",
+                        Content = "Internal error:  please report"
+                    };
                 }
-
-                var match = grammar.Match(body.Rule, body.Text);
-
-                if (match == null)
-                {
-                    return new BadRequestObjectResult("Text cannot be matched by grammar");
-                }
-
-                var outputModel = new AnonymousAnalysisOutputModel(match);
-                var output = JsonSerializerHelper.Serialize(outputModel);
-
-                return new ContentResult
-                {
-                    StatusCode = 200,
-                    ContentType = "application/json",
-                    Content = output
-                };
             }
-            catch (ParsingException ex)
+            catch (Exception ex)
             {
-                return new BadRequestObjectResult(ex.Message);
-            }
-            catch
-            {
-                return new ContentResult
-                {
-                    StatusCode = 500,
-                    ContentType = "text/plain",
-                    Content = "Internal error:  please report"
-                };
+                log.Error($"Body parsing error:  {ex.GetType().Name}, '{ex.Message}'");
+
+                return new BadRequestObjectResult("JSON body isn't conform");
             }
         }
     }
