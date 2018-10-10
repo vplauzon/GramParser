e9d6e2cd5a22103ec12c40bed18927cc6e832010
vplauzon <vplauzon@vplauzon.com>
2018-10-10 16:48:30 -0400

    Add Root Controller

---
+++ PasWebApi/Controllers/RootController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PasLib;
+using PasWebApi.Models.AnonymousAnalysis;
+
+namespace PasWebApi.Controllers
+{
+    /// <summary>Used for probes.</summary>
+    [Route("/")]
+    public class RootController : Controller
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return new ObjectResult(new
+            {
+                Status = "Ready"
+            });
+        }
+    }
+}
