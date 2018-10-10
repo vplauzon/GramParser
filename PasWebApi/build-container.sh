44e768352ba2dafbbf4849efb3c7e3d4084bb651
vplauzon <vplauzon@vplauzon.com>
2018-10-10 18:13:12 -0400

    .

---
+++ PasWebApi/build-container.sh
@@ -0,0 +1,8 @@
+﻿#	Build docker container
+sudo docker build -t vplauzon/pas-web-api .
+
+#	Publish image
+#sudo docker push vplauzon/pas-web-api
+
+#	Test image
+#sudo docker run -it vplauzon/cosmos-db-target-config bash
