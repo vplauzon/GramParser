2bacc31bc13d6d5d20cdd678db6508c704a8d934
Vincent-Philippe Lauzon <vincent_lauzon@yahoo.com.au>
2018-10-11 17:51:30 -0400

    .

--- build-container.sh
+++ build-container.sh
@@ -5,6 +5,8 @@ sudo docker build -t vplauzon/pas-web-api .
 #sudo docker push vplauzon/pas-web-api
 
 #	Test image
-#sudo docker run -d -p 4000:80 vplauzon/pas-web-api
+#sudo docker run --name test-web-api -d -p 4000:80 vplauzon/pas-web-api:<build version>
 #curl localhost:4000
 #sudo docker run -it vplauzon/pas-web-api -p 4000:80 bash
+#  Clean up after test
+#sudo docker stop test-web-api && sudo docker container prune -f
--- inject-version.sh
+++ inject-version.sh
@@ -1,5 +1,12 @@
-#	Fetch first parameter
-version=$1
+#  Fetch first parameter
+version=$(printenv BUILD_VERSION)
 
-#	Simple find / replace
-sed -i -e 's/BUILD_VALUE/$version/g' PasWebApi/ApiVersion.cs
+echo "Build Version:  $version"
+
+#  Build sed command
+sedcmd="s/BUILD_VALUE/$version/g"
+
+echo "sed command:  $sedcmd"
+
+#  Simple find / replace
+sed -i -e $sedcmd PasWebApi/ApiVersion.cs
