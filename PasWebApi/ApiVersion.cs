2f36aba5c1d0f1dedd01a8a1837b8470c757ec46
Vincent-Philippe Lauzon <vincent_lauzon@yahoo.com.au>
2018-10-12 17:22:34 -0400

    .

--- PasWebApi/ApiVersion.cs
+++ PasWebApi/ApiVersion.cs
@@ -6,10 +6,10 @@ namespace PasWebApi
 {
     internal static class ApiVersion
     {
-        public static string Number { get { return "0.0.98"; } }
+        public static string Number { get { return "API_VERSION_VALUE"; } }
 
-        public static string Build { get { return "BUILD_VALUE"; } }
+        public static string Build { get { return "BUILD_NUMBER_VALUE"; } }
 
-        public static string FullVersion { get { return $"{Number}.{Build}"; } }
+        public static string FullVersion { get { return $"FULL_VERSION_VALUE"; } }
     }
 }
