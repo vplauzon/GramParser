042f95d02f7447c5d0c3a8a14e9dc196c325732c
vplauzon <vplauzon@vplauzon.com>
2018-11-08 10:12:26 -0500

    .

--- deployment/release-scripts/fetch-instrumentation-key.sh
+++ deployment/release-scripts/fetch-instrumentation-key.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/bash
 
-$rg=$1
+rg=$1
+ai=$2
 
 echo "Resource Group:  $rg"
+echo "App Insight:  $ai"
 
-$key=$(az resource show -g $rg -n central-insight --resource-type "microsoft.insights/components" --query properties.InstrumentationKey -o tsv)
+$key=$(az resource show -g $rg -n $ai --resource-type "microsoft.insights/components" --query properties.InstrumentationKey -o tsv)
 
 echo "Instrumentation Key:  $key"
 
