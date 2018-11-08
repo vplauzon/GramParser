81e8704606e1bc900c10e875d1f7e920dd82f08b
vplauzon <vplauzon@vplauzon.com>
2018-11-08 09:54:39 -0500

    .

--- deployment/release-scripts/fetch-instrumentation-key.sh
+++ deployment/release-scripts/fetch-instrumentation-key.sh
@@ -2,6 +2,10 @@
 
 $rg=$1
 
+echo "Resource Group:  $rg"
+
 $key=$(az resource show -g $rg -n central-insight --resource-type "microsoft.insights/components" --query properties.InstrumentationKey -o tsv)
+
+echo "Instrumentation Key:  $key"
 
 echo "##vso[task.setvariable variable=instrumentationKey;]$key"
