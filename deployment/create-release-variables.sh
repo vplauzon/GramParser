34d1d5632ad688764dd79f8c258f7f1589cdde9e
vplauzon <vplauzon@vplauzon.com>
2018-11-10 08:26:19 -0500

    Run tests within Docker file

---
+++ deployment/create-release-variables.sh
@@ -0,0 +1,21 @@
+﻿#!/bin/bash
+
+#   Should be $(Release.EnvironmentName)
+environment=$1
+
+appinsight="central-insight-$environment"
+cluster="cluster-$environment"
+group="shared-infra-$environment"
+repo="vplauzon/pas-api"
+tag="$environment"
+
+echo "appinsight:  '$appinsight'"
+echo "##vso[task.setvariable variable=appinsight;]$appinsight"
+echo "appinsight:  '$cluster'"
+echo "##vso[task.setvariable variable=cluster;]$cluster"
+echo "appinsight:  '$group'"
+echo "##vso[task.setvariable variable=group;]$group"
+echo "appinsight:  '$repo'"
+echo "##vso[task.setvariable variable=repo;]$repo"
+echo "appinsight:  '$tag'"
+echo "##vso[task.setvariable variable=tag;]$tag"
---
+++ deployment/retrieve-full-version.sh
@@ -0,0 +1,10 @@
+﻿#!/bin/bash
+
+#   Should be $(System.DefaultWorkingDirectory)/_API/drop/FullVersion.txt
+fullVersionPath=$1
+
+fullVersion=$(cat $fullVersionPath)
+
+echo "Full Version:  '$fullVersion'"
+
+echo "##vso[task.setvariable variable=full-version;]$fullVersion"
