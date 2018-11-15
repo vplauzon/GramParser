549da02a738c1727e14ccf1de73203b63382d234
Vincent-Philippe Lauzon <vincent_lauzon@yahoo.com.au>
2018-11-15 09:42:11 -0500

    .

--- PasApiClient/ParserClient.cs
+++ PasApiClient/ParserClient.cs
@@ -2,7 +2,27 @@
 
 namespace PasApiClient
 {
+    /// <summary>Client component for accessing Parser as a Service.</summary>
     public class ParserClient
     {
+        private static readonly Uri DEFAULT_BASE_URI = new Uri("http://pas-api.vplauzon.com");
+        private readonly Uri _baseUri;
+
+        #region Constructors
+        public static ParserClient Create()
+        {
+            return CreateFromBaseUrl(DEFAULT_BASE_URI);
+        }
+
+        public static ParserClient CreateFromBaseUrl(Uri baseUri)
+        {
+            return new ParserClient(baseUri);
+        }
+
+        private ParserClient(Uri baseUri)
+        {
+            _baseUri = baseUri ?? throw new ArgumentNullException(nameof(baseUri));
+        }
+        #endregion
     }
 }
