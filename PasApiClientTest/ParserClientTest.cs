00090013092f4a535f97cfd8c227597a1d545588
Vincent-Philippe Lauzon <vincent_lauzon@yahoo.com.au>
2018-11-15 09:35:05 -0500

    Add ParserClient

---
+++ PasApiClientTest/ParserClientTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PasApiClientTest
+{
+    [TestClass]
+    public class ParserClientTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
