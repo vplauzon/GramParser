6364750e91d0fed458c62eb50a07bd4f0a5c82bc
Vincent-Philippe Lauzon <vincent_lauzon@yahoo.com.au>
2019-09-07 17:55:27 -0400

    Add ThisExtractor

---
+++ PasLib/ThisExtractor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Text;
+
+namespace PasLib
+{
+    internal class ThisExtractor : IOutputExtractor
+    {
+        object IOutputExtractor.ExtractOutput(SubString text, IImmutableDictionary<string, RuleMatch> namedChildren)
+        {
+            return text;
+        }
+    }
+}
