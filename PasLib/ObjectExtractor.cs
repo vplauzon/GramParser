76183056e64ddb132bdb1529cdf5f4332e2c0736
Vincent-Philippe Lauzon <vincent_lauzon@yahoo.com.au>
2019-09-10 07:34:34 -0400

    Half implement the object-extractor

---
+++ PasLib/ObjectExtractor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace PasLib
+{
+    internal class ObjectExtractor : IOutputExtractor
+    {
+        private readonly IImmutableList<KeyValuePair<IOutputExtractor, IOutputExtractor>>
+            _extractorPairs;
+
+        public ObjectExtractor(
+            IEnumerable<KeyValuePair<IOutputExtractor, IOutputExtractor>> pairs)
+        {
+            _extractorPairs =
+               ImmutableList<KeyValuePair<IOutputExtractor, IOutputExtractor>>
+               .Empty
+               .AddRange(pairs);
+        }
+
+        object IOutputExtractor.ExtractOutput(
+            SubString text,
+            IImmutableDictionary<string, RuleMatch> namedChildren)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
