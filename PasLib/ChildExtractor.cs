41444e9a37ee5733dace03e9561bbb42f48317f0
Vincent-Philippe Lauzon <vincent_lauzon@yahoo.com.au>
2019-09-19 14:39:01 -0400

    Add ChildExtractor

---
+++ PasLib/ChildExtractor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Immutable;
+
+namespace PasLib
+{
+    internal class ChildExtractor : IOutputExtractor
+    {
+        private readonly string _name;
+
+        public ChildExtractor(string name)
+        {
+            _name = name;
+        }
+
+        object IOutputExtractor.ExtractOutput(
+            SubString text,
+            IImmutableDictionary<string, RuleMatch> namedChildren)
+        {
+            RuleMatch child;
+
+            if (namedChildren.TryGetValue(_name, out child))
+            {
+                return child.Output;
+            }
+            else
+            {
+                var childList = string.Join(", ", namedChildren.Keys);
+
+                throw new ParsingException(
+                    $"Can't find child '{_name}' for match '{text.ToString()}', only "
+                    + $"{{{childList}}}");
+            }
+        }
+    }
+}
