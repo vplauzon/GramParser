c44447f8f956cf4f773bd576fd98ed29586607a6
vplauzon <vplauzon@vplauzon.com>
2020-09-17 19:03:07 -0400

    .

--- deployment/patch-nuget-version.py
+++ deployment/patch-nuget-version.py
@@ -8,21 +8,26 @@ def writeXml(document, path):
     with open(path, "w" ) as fs:  
         fs.write(document.toxml() ) 
 
-if len(sys.argv) != 3:
+if len(sys.argv) != 4:
     print("There are %d arguments" % (len(sys.argv)-1))
     print("Arguments should be")
     print("1-File path")
     print("2-Patch number")
+    print("3-Alpha (true or false)")
 else:
     path = sys.argv[1]
     patchNumber = sys.argv[2]
-    alphaPath = os.path.dirname(path)+"/alpha-nuget.csproj"
-    goldPath = os.path.dirname(path)+"/gold-nuget.csproj"
+    isAlpha = sys.argv[3]=='true'
+
+    nugetPath = os.path.dirname(path)+"/nuget.csproj"
+    if isAlpha:
+        suffix = '-alpha'
+    else:
+        suffix = ''
 
     print ('Text file:  %s' % (path))
     print ('Patch Number:  %s' % (patchNumber))
-    print ('Alpha path:  %s' % (alphaPath))
-    print ('Gold path:  %s' % (goldPath))
+    print ('Nuget path:  %s' % (nugetPath))
 
     #   Load XML and print out
     document = md.parse(path)
@@ -35,17 +40,10 @@ else:
 
     #   Add patch to the version
     version=document.getElementsByTagName('Version')[0]
-    version.firstChild.nodeValue += "." + patchNumber
+    version.firstChild.nodeValue += "." + patchNumber + suffix
 
-    #   Output gold
-    print('Gold content:')
+    #   Output project
+    print('Nuget project content:')
     print(document.toxml())
-    writeXml(document, goldPath)
+    writeXml(document, nugetPath)
 
-    #   Add '-alpha' to the version
-    version.firstChild.nodeValue += "-alpha"
-
-    #   Output alpha
-    print('Alpha content:')
-    print(document.toxml())
-    writeXml(document, alphaPath)
