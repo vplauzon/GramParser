e5aa7ebbfa046350d556c17eb794d9c4a6f7b99c
vplauzon <vplauzon@vplauzon.com>
2021-01-02 21:33:41 -0500

    Move the app-version to the dll version

---
+++ code/GramParserWorkbench/Models/AppVersionHelper.cs
@@ -0,0 +1,13 @@
+﻿using GramParserLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GramParserWorkbench.Models
+{
+    public static class AppVersionHelper
+    {
+        public static string ParserVersion => typeof(MetaGrammar).Assembly.GetName()!.Version!.ToString();
+    }
+}
