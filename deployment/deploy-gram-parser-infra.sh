8671734a3e58369c2a34fc2b9e8d5e892b6f5007
vplauzon <vplauzon@vplauzon.com>
2021-02-01 18:23:34 -0500

    Fix customDomainVerificationId

--- deployment/deploy-gram-parser-infra.sh
+++ deployment/deploy-gram-parser-infra.sh
@@ -6,8 +6,10 @@
 ##  Parameters:
 ##
 ##  1- Name of resource group
+##  2- Value for customDomainVerificationId
 
 rg=$1
+verificationId=$2
 
 echo "Resource group:  $rg"
 echo "Current directory:  $(pwd)"
@@ -16,4 +18,5 @@ echo
 echo "Deploying ARM template"
 
 az deployment group create -n "deploy-$(uuidgen)" -g $rg \
-    --template-file gram-parser-infra.json
+    --template-file gram-parser-infra.json \
+    --parameters verificationId=$verificationId
