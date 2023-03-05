d6ba6d5e96cd7c57a7dbd15d0733d8be9ff76db0
vplauzon <vincent_lauzon@yahoo.com.au>
2023-03-05 09:04:38 -0500

    Propagate rule properties in wrapped rules

--- code/GramParserLib/Rule/WrappedRule.cs
+++ code/GramParserLib/Rule/WrappedRule.cs
@@ -5,41 +5,35 @@ using System.Text;
 
 namespace GramParserLib.Rule
 {
-    internal class WrappedRule : RuleBase
+    internal class WrappedRule : IRule
     {
+        private readonly string? _ruleName;
         private readonly IRule _referencedRule;
 
         public WrappedRule(
             string? ruleName,
             IRuleOutput? ruleOutput,
-            IRule referencedRule,
-            bool? hasInterleave = null,
-            bool? isRecursive = null,
-            bool? isCaseSensitive = null)
-            : base(
-                  ruleName,
-                  ruleOutput,
-                  hasInterleave,
-                  isRecursive,
-                  isCaseSensitive)
+            IRule referencedRule)
         {
+            _ruleName = ruleName;
             _referencedRule = referencedRule;
         }
 
-        public override bool IsTerminalRule => false;
+        #region IRuleProperties
+        public bool? HasInterleave => _referencedRule.HasInterleave;
 
-        protected override IEnumerable<RuleMatch> OnMatch(ExplorerContext context)
+        public bool? IsRecursive => _referencedRule.IsRecursive;
+
+        public bool IsTerminalRule => false;
+
+        public bool? IsCaseSensitive => _referencedRule.IsCaseSensitive;
+        #endregion
+
+        public string? RuleName => _ruleName;
+
+        public IEnumerable<RuleMatch> Match(ExplorerContext context)
         {
-            var matches = _referencedRule.Match(context);
-            var wrapMatches = from m in matches
-                              select new RuleMatch(
-                                  this,
-                                  m.Text,
-                                  () => RuleOutput.ComputeOutput(
-                                      m.Text,
-                                      new Lazy<object?>(() => m.ComputeOutput())));
-            
-            return wrapMatches;
+            return _referencedRule.Match(context);
         }
     }
 }
